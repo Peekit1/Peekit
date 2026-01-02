8981a9c4d91cbaf6db26d53ba72c7e1aec1f3b34
Peekit1 <axelarsayepro@gmail.com>
2026-01-02 01:20:38 +0100

    Update ProjectDetails.tsx

--- components/ProjectDetails.tsx
+++ components/ProjectDetails.tsx
@@ -209,8 +209,8 @@ export const ProjectDetails: React.FC<ExtendedProjectDetailsProps> = ({
     const baseUrl = window.location.href.split('#')[0]; 
     const clientLink = `${baseUrl}#/v/${project.id}`;
     try {
-        const SERVICE_ID = "service_service_vlelgtd"; 
-        const TEMPLATE_ID = "template_template_mjzqkyl"; 
+        const SERVICE_ID = "service_vlelgtd"; 
+        const TEMPLATE_ID = "template_mjzqkyl"; 
         const PUBLIC_KEY = "3l-ZU5KwqK1qV2W1j"; 
         await emailjs.send(SERVICE_ID, TEMPLATE_ID, {
             to_email: project.clientEmail, client_name: project.clientName, subject: notificationEmail.subject, message: notificationEmail.body, link: clientLink, studio_name: studioName
