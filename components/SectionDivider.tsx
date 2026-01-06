98fdbd59d1e32cc7540375127ad9cc076a693b5d
Peekit1 <axelarsayepro@gmail.com>
2026-01-06 17:53:19 +0100

    Update SectionDivider.tsx

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 export const SectionDivider: React.FC = () => {
   return (
-    <div className="relative w-full h-32 flex items-center justify-center overflow-hidden pointer-events-none">
-      {/* Fond dégradé subtil pour lier les sections */}
-      <div className="absolute inset-0 bg-gradient-to-b from-white via-gray-50/50 to-white"></div>
-      
-      {/* Ligne verticale très fine et élégante au centre */}
-      <div className="absolute h-full w-px bg-gradient-to-b from-transparent via-gray-200 to-transparent"></div>
-    </div>
+    // Simple espace blanc de 120px (h-32) pour aérer sans couper
+    <div className="w-full h-24 md:h-32 bg-white"></div>
   );
 };
